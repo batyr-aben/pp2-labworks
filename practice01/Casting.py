f5b8a7185ced9796384a509457bf19bb510f8adb
batyr-aben <b_aben@kbtu.kz>
2026-01-30 22:00:20 +0500

    Casting.py

---
+++ practice01/Casting.py
@@ -0,0 +1,6 @@
+x = float(4) # x will be 4.0
+y = int(56346.4) # y will be 56346
+z = float("3") # z will be 3
+a = str(4) # a will be '4'
+b = str(3.0) # b will be '3.0'
+c = complex(3) # c will be 3 + 0j
