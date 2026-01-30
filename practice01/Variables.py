53187c0fa709cb90cda2b3b5e788dac2177c3e35
batyr-aben <b_aben@kbtu.kz>
2026-01-30 21:41:26 +0500

    Variables.py

---
+++ practice01/Variables.py
@@ -0,0 +1,23 @@
+x = 4 # x is int and equals 4
+x = "Marcus" # x is string and equals Marcus
+print(x) # will print out Sally
+
+x = 5 # x is int and equals 5
+y = "Patrick" # y is string and equals 5
+print(x) # will print out 5
+print(y) # will print out Patrick
+
+x, y, z = "Alice", "John", "Connor" # assigning mulitple variables in one line
+print(x) will print out Alice
+print(y) will print out John
+print(z) will print out Connor
+
+x = y = z = 5 # assigning multiple variables to the same value
+print(x) # 5
+print(y) # 5
+print(z) # 5
+
+x = "Python "
+y = "is "
+z = "amazing "
+print(x + y + z) # Python is amazing
