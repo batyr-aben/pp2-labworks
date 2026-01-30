35c508c63bee5185bd00584f1558f230fc2393de
batyr-aben <b_aben@kbtu.kz>
2026-01-30 21:30:46 +0500

    Syntax.py

---
+++ practice01/Syntax.py
@@ -0,0 +1,14 @@
+if 5 > 3:
+  print("Five is greater than tree")
+
+if 3 < 4:
+  print("Three is less than four")
+
+if 4 == 4:
+  print("True")
+
+if 1 > 5:
+  print("False")
+
+if 5 < 6:
+  print("Five is less than six")
