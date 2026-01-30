bce4302410c5a46757978110b7cf5b87e9687dcb
batyr-aben <b_aben@kbtu.kz>
2026-01-30 21:46:16 +0500

    DataType.py

---
+++ practice01/DataType.py
@@ -0,0 +1,12 @@
+x = "Hello" 
+y = 20
+z = 40.5
+a = ["apple", "burger", "pizza"]
+f = {"name" : "Jack", "age" : 99}
+e = True
+print(type(x)) # string
+print(type(y)) # int
+print(type(z)) # float
+print(type(a)) # list
+print(type(f)) # dictionary
+print(type(e)) # boolean
