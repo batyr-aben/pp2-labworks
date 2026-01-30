a75d2ab17f4326a9d8597b28cc8c05ebdcc8b866
batyr-aben <b_aben@kbtu.kz>
2026-01-30 21:55:26 +0500

    Numbers.py

---
+++ practice01/Numbers.py
@@ -0,0 +1,32 @@
+x = 1
+y = 4353454
+z = -234234
+print(type(x)) # integer
+print(type(y)) # integer
+print(type(z)) # integer
+
+a = 10.5
+b = 4.6
+c = -7.4
+print(type(a)) # float
+print(type(b)) # float
+print(type(c)) # float
+
+d = 3 + 5j
+e = 5j
+f = -10j
+
+print(type(d)) # complex
+print(type(e)) # complex
+print(type(f)) # complex
+
+num1 = 4 # integer
+num2 = 9.5 # float
+num3 = 2j # complex
+
+convert1 = float(num1)
+convert2 = int(num2)
+
+print(convert1) # 4.0
+print(convert2) # 9
+
