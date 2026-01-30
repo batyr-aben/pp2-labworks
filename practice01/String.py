858e5c9ec34d6ec4ced6472e84fd1385f7c7beab
batyr-aben <b_aben@kbtu.kz>
2026-01-30 22:08:14 +0500

    String.py

---
+++ practice01/String.py
@@ -0,0 +1,26 @@
+x = "Hello"
+y = 'Hello'
+print(x) # Hello
+print(y) # Hello
+
+z = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua""" # multiline string
+
+print(z) # will print out z
+
+b = "Hello"
+print(b[0:2]) # He
+print(b[::-1]) # olleH
+
+c = "Hi"
+print(c.upper()) # HI
+
+d = "CAPS"
+print(d.lower()) # caps
+
+e = "Hello, World!"
+print(a.split(",")) # ['Hello', ' World!']
+
+
