e450310e84bd70e1372dc4819b23ab511687627d
batyr-aben <b_aben@kbtu.kz>
2026-02-01 20:17:42 +0500

    BooleanValues

---
+++ practice02/BooleanValues.py
@@ -0,0 +1,6 @@
+print(10 > 9) # True
+print(10 == 9) # False
+print(10 < 9) # False
+print(5 == 5) # True
+print(bool("Hello")) # True, because string is not empty
+print(bool("")) # False, because string is empty
