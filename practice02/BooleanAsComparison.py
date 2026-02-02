6c00092e36c8fc33428aabe30ddc07fd1e8395a3
batyr-aben <b_aben@kbtu.kz>
2026-02-02 20:17:21 +0500

    BooleanAsComparison

---
+++ practice02/BooleanAsComparison.py
@@ -0,0 +1,34 @@
+if (5 > 3):
+  print("True")
+else:
+  print("False")
+
+
+if (5 == 5):
+  print("Five is equal to five")
+else:
+  print("False")
+
+x = 54
+y = 23
+if (x != y):
+  print("x is not equal to y")
+else:
+  print("x is equal to y")
+
+x = 34
+y = 90
+z = 45
+if (x < y AND z < y):
+  print("y is the largest number")
+else:
+  print("y is not the largest number")
+
+a = 63
+b = 34
+c = 12
+if (a > b OR a > c):
+  print("B or C is less than A")
+else:
+  print("No")
+  
