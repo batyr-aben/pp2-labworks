20c870f76f03afcf96b150f08b1ec13747de19a9
batyr-aben <b_aben@kbtu.kz>
2026-02-05 16:50:01 +0500

    BooleanOperators

---
+++ practice02/BooleanOperators.py
@@ -0,0 +1,14 @@
+x = 5
+print(x > 0 and x < 10) # True
+
+x = 4 
+print(x < 5 and x > 10) # False, because 4 is not greater than 10
+
+x = 10
+print(not(x > 3 and x < 11)) # False, because the result was inverted
+
+print(5 < 7 and 5 > 7) # False
+
+a = 4
+b = 4
+print(a == b) # True
