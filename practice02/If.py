32c891a321ac2310ebbf7589d4803982582da6fc
batyr-aben <b_aben@kbtu.kz>
2026-02-05 16:54:34 +0500

    If

---
+++ practice02/If.py
@@ -0,0 +1,22 @@
+a = 33
+b = 200
+if a < b:
+  print("a is less than b")
+
+num = 13
+if num > 0:
+  print("This number is positive")
+
+age = 19
+if age >= 18:
+  print("You are 18 years old, you can enter")
+  print("You are an adult")
+
+is_logged_in = True
+if is_logged_in:
+  print("Welcome back")
+
+num1 = 13
+num2 = 14
+if num1 > num2:
+  print("Number 1 is greater than number 2")
