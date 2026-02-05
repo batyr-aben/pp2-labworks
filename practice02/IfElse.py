77ab5b489de40bd81a62472c891c54734b8cc8ce
batyr-aben <b_aben@kbtu.kz>
2026-02-05 17:08:30 +0500

    IfElse

---
+++ practice02/IfElse.py
@@ -0,0 +1,33 @@
+a = 100
+b = 324
+if a > b:
+  print("a is greater than b")
+else:
+  print("b is greater than a")
+
+num = 5
+if num % 2 == 0:
+  print("This number is even")
+else:
+  print("This number is odd")
+
+username = "John"
+if len(username) > 0:
+  print(f"Hello, {username}")
+else:
+  print("Please enter your username")
+
+a = 13
+b = 23
+c = 3215
+if c > a and c > b:
+  print("c is the largest number")
+else:
+  print("c is not the largest number")
+
+num1 = 23
+num2 = 22
+if num1 == num2:
+  print("Both numbers are equal to each other")
+else:
+  print("These are two different numbers")
