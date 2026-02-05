c73e1026a17828b64ea4017a28a7f58e5876c1bd
batyr-aben <b_aben@kbtu.kz>
2026-02-05 18:06:11 +0500

    ShortHandIf

---
+++ practice02/ShortHandIf.py
@@ -0,0 +1,21 @@
+x = 90
+y = 80
+max_value = x if x > y else y
+print(f"Maximum value: {max_value}")
+
+a = 330 
+b = 330
+print("A") if a > b else print("=") if a == b else print("B")
+
+num1 = 234
+num2 = 567
+bigger = num1 if num1 > num2 else num2
+print(f"Bigger is {bigger}")
+
+x = 2
+y = 4095
+print("x") if x > y else print("y")
+
+a = 5
+b = 2
+if a > b: print("a is greater than b")
