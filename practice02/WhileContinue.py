c35d5fcab1729b25939605a67fa8e1b9c960500a
batyr-aben <b_aben@kbtu.kz>
2026-02-06 15:58:14 +0500

    WhileContinue.py

---
+++ practice02/WhileContinue.py
@@ -0,0 +1,38 @@
+i = 0
+while i <= 5:
+    if i == 3:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+i = 0
+while i < 10:
+    i += 1
+    if i == 5:
+        continue
+    print(i)
+
+i = 0
+while i <= 10:
+    i += 1
+    if i % 2 == 0:
+        continue
+    print(i)
+
+i = 0
+while i <= 10:
+    if i % 3 != 0:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
+i = 0
+while i <= 5:
+    if i == 2:
+        i += 1
+        continue
+    print(i)
+    i += 1
+
