3580a724360362a11f1cbe811791a11847ad0b50
batyr-aben <b_aben@kbtu.kz>
2026-02-06 13:15:13 +0500

    For.py

---
+++ practice02/For.py
@@ -0,0 +1,16 @@
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  print(x)
+
+for x in "ape":
+  print(x)
+
+for i in range(1,3):
+  print(i)
+
+for i in range(1,10,2):
+  print(i)
+
+foods = ["burger", "pizza", "apple", "soup"]
+for food in foods:
+  print(food)
