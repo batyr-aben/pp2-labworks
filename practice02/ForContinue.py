a164c081755d75daaf9cd27fd543753b2dc76847
batyr-aben <b_aben@kbtu.kz>
2026-02-06 13:50:40 +0500

    ForContinue

---
+++ practice02/ForContinue.py
@@ -0,0 +1,25 @@
+for letter in 'Django':    
+    if letter == 'D':
+        continue
+    print("Current Letter: " + letter)
+
+for i in range(1, 4):
+  if i == 1:
+    continue
+  print(i)
+
+fruits = ["apple", "fish", "banana"]
+for fruit in fruits:
+  if fruit == "fish":
+    continue
+  print(fruit)
+
+for x in "Ape":
+  if x == "":
+    continue
+  print(x)	
+
+for i in range(1, 10, 2):
+  if i == 2:
+    continue
+  print(i)
