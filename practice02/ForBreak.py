38fe4ac4ab7938b8149ab12c12e93dd8882cbe6e
batyr-aben <b_aben@kbtu.kz>
2026-02-06 13:34:45 +0500

    ForBreak.py

---
+++ practice02/ForBreak.py
@@ -0,0 +1,26 @@
+fruits = ["banana", "apple", "strawberry"]
+for fruit in fruits:
+  print(fruit)
+  if fruit == "apple":
+    break
+
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+  if x == "banana":
+    break
+  print(x)
+
+for i in range(5):
+  print(i)
+  if i == 4:
+    break
+
+for i in range(2, 9):
+  if == 4:
+    break
+  print(i)
+
+for i in range(2, 10, 2):
+  print(i)
+  if i == 5:
+    break
