b0a4eeb432587efaba64c1dffb3c096002397999
batyr-aben <b_aben@kbtu.kz>
2026-02-28 10:20:47 +0500

    Date.py

---
+++ practice04/date.py
@@ -0,0 +1,47 @@
+from datetime import datetime
+
+today = datetime.now()
+print(today)
+    
+
+from datetime import datetime
+
+birth_year = int(input("Введите год рождения: "))
+current_year = datetime.now().year
+
+age = current_year - birth_year
+print(age)
+
+from datetime import date
+
+today = date.today()
+
+next_year = today.year + 1
+new_year = date(next_year, 1, 1)
+
+difference = new_year - today
+
+print(f"Days left till New Year {differnce.days}")
+
+from datetime import datetime
+
+date1 = input()
+date2 = input()
+
+d1 = datetime.strptime(date1, "%d.%m.%Y")
+d2 = datetime.strptime(date2, "%d.%m.%Y")
+
+difference = d1 - d2
+
+print(abs(difference.days)))
+
+
+from datetime import datetime, timedelta
+
+user_date = input()
+
+d = datetime.strptime(user_date, "%d.%m.%Y")
+
+new_date = d + timedelta(days=30)
+
+print(new_date.strftime("%d.%m.%Y"))
