ae8d57a0b0a424385afe9fd2401d2960b1cf359b
batyr-aben <b_aben@kbtu.kz>
2026-02-28 09:18:16 +0500

    Math.py

---
+++ practice04/math.py
@@ -0,0 +1,24 @@
+import math
+x = math.pi
+print(x) # will print out pi
+
+y = math.sqrt(64)
+print(y) # 8
+
+z = math.ceil(1.1)
+print(z) # 2
+
+w = math.floor(1.9)
+print(w) # 1
+
+a = min(1, 2, 3)
+print(a) # 1
+
+b = max(12, 34, 56)
+print(b) # 56
+
+c = abs(-9)
+print(c) # 9
+
+d = pow(4, 4)
+print(d) # 256
