30fd401316bf07d36ff428016ec6f640bbef2aec
batyr-aben <b_aben@kbtu.kz>
2026-03-21 11:31:10 +0500

    delete

--- practice06/file_handling/read_files.py
+++
@@ -1,7 +0,0 @@
-with open("sample.txt", "w") as s:
-    s.write("Hello, world!\n")
-    s.write("This is a sample file\n")
-
-with open("sample.txt", "r") as s:
-    content = s.read()
-    print(content)
--- practice06/file_handling/write_files.py
+++
@@ -1,6 +0,0 @@
-with open("sample.txt", "w") as s:
-    s.write("Hello, world!\n")
-    s.write("This is a sample file\n")
-
-with open("sample1.txt", "w") as s1:
-    s1.write("This is a second file\n")
